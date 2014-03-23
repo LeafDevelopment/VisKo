8f28ba78a90dfbba6ae17ad74b3b53afdd514fba
Maria <mmcortes@miners.utep.edu>
2014-03-23 12:10:24 -0600

    PHP/JavaBridge Sample

---
+++ PHP:JavaBridge Sample/VisKo/src/example2/TestThis2.java
@@ -0,0 +1,14 @@
+package example2;
+
+public class TestThis2 {
+
+	public TestThis2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String test() {
+		// TODO Auto-generated method stub
+		return "Bitch";
+	}
+
+}
