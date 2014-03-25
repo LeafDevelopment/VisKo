c1b524738fabcf1509c9abdda6f1e622cd9fc893
maflores16 <maflores16@miners.utep.edu>
2014-03-24 18:36:50 -0600

    i dunno what i did

--- SourceCode/v1/CommonUser/Visualize/addVisualization.js
+++ SourceCode/v1/CommonUser/Visualize/addVisualization.js
@@ -1,11 +1,9 @@
 
 
-function addToList()
+function addToList(i)
 {
-    
-    
-        document.write('<tr><td>'+(1)+'</td><td>'+sessvars.abstraction.name+
-                '</td><td>'+sessvars.abstraction.inputDataFormat
+        document.write('<tr><td>'+(i+1)+'</td><td>'+sessvars.abstraction[i].name+
+                '</td><td>'+sessvars.abstraction[i].inputDataFormat
             +'</td><td><button type="button">Edit</button></td><td><button type="button">Run</button></td></tr>');
 }
 
