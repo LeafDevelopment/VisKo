51f82055d7a1cba9ed5fafe91f4808301d94da95
Maria <mmcortes@miners.utep.edu>
2014-03-24 22:14:32 -0600

    Included Mario's commonVisualization into the GUI
    To include all your html just follow this example
    
    Notice, you also have to change, ./Visualize/ANY.CSS in your normal
    change, because php is crazy about it.

---
+++ SourceCode/v1/CommonUser/visualize.php
@@ -0,0 +1,43 @@
+	<!--Import Header with VisKo logo-->
+<?php
+	require_once("regHeader.inc");
+?>
+     
+<!-- start visko-->
+<link rel="stylesheet" type="text/css" href="style1.css" media="screen" />
+
+
+<div id="wrapper">
+<div id="main_container">
+  <div>
+	<br/><br/>  
+	<!-- Import Navigation side bar for regular user-->
+	      
+<?php 
+ require_once("regNavigation.inc");
+	?>
+
+<!--Body of regular user home page-->
+  <div id="middle_box">
+    <div class="middle_box_content">
+
+	<?php
+		//Import Mario's page 
+		require_once("./Visualize/commonVisualization.html");
+	?>
+
+
+
+
+
+     </div>
+  </div>
+
+</div>
+
+ <div id="middle_box">
+
+<!-- Import footer end of visko-->
+<?php
+	require_once("footer.inc"); 
+?>
