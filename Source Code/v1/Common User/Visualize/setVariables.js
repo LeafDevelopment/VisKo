70d5a513fa914677f75e21b399ebdd4d9bf32de7
maflores16 <maflores16@miners.utep.edu>
2014-03-24 18:13:30 -0600

    Visualize update

--- Source Code/v1/Common User/Visualize/setVariables.js
+++ Source Code/v1/Common User/Visualize/setVariables.js
@@ -1,24 +1,26 @@
 function setAbstraction(newAbstraction)
 {
-    sessvars.abstraction = {abstraction: newAbstraction };
+    sessvars.abstraction[sessvars.count.count] = {abstraction: newAbstraction };
     
-    var abs = sessvars.abstraction.abstraction.split("_",2);
-    sessvars.abstraction.name = abs[1];
+    var abs = sessvars.abstraction[sessvars.count.count].abstraction.split("_",2);
+    sessvars.abstraction[sessvars.count.count].name = abs[1];
     
-    var someimage = document.getElementById(newAbstraction)
+    var someimage = document.getElementById(newAbstraction);
     var myimg = someimage.getElementsByTagName('img')[0];
     var image = myimg.src;
-    sessvars.abstraction.image = image;
+    sessvars.abstraction[sessvars.count.count].image = image;
     
     location.href = 'setQuery.html';
 }
 
 function setQuery(viewerSet, inputDataFormat, inputDataType, inputDataURL)
 {
-    sessvars.abstraction.viewerSet = viewerSet;
-    sessvars.abstraction.inputDataFormat = inputDataFormat;
-    sessvars.abstraction.inputDataType = inputDataType;
-    sessvars.abstraction.inputDataURL = inputDataURL;
+    sessvars.abstraction[sessvars.count.count].viewerSet = viewerSet;
+    sessvars.abstraction[sessvars.count.count].inputDataFormat = inputDataFormat;
+    sessvars.abstraction[sessvars.count.count].inputDataType = inputDataType;
+    sessvars.abstraction[sessvars.count.count].inputDataURL = inputDataURL;
+    
+    sessvars.count.count++;
         
     location.href = 'setPipelines.html';
 }
